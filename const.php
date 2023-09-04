f2f3e704f8d0fd4a9c9559d332eee85832fd270a
Estevão P. Rolim <estevao.prolim@gmail.com>
2023-09-04 16:55:15 +0100

    minor changes to site information and const

--- const.php
+++ const.php
@@ -1,15 +1,12 @@
 <?php
 
-function is_subdomain() {
-    return dirname($_SERVER['SCRIPT_NAME']) == '/';
-}
+define('IS_SUBDOMAIN', true);
 
 $root_script_name = basename(__DIR__);
 
-
 // error_reporting(0);
 
-define('BASE_REQUEST', is_subdomain() ? '' : $root_script_name);
+define('BASE_REQUEST', IS_SUBDOMAIN ? '' : $root_script_name);
 
 define('BASE_DIR', __DIR__);
 define('VIEW_DIR', BASE_DIR . '/views');
@@ -18,7 +15,7 @@ define('ADMIN_DIR', BASE_DIR . '/admin');
 define('CONTROL_DIR', ADMIN_DIR . '/control');
 
 define('DEFAULT_HTTP', isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http');
-define('BASE_URL', DEFAULT_HTTP . '://' . $_SERVER['SERVER_NAME'] . '/' . $root_script_name);
+define('BASE_URL', DEFAULT_HTTP . '://' . $_SERVER['SERVER_NAME'] . '/' . BASE_REQUEST);
 define('VIEW_URL', BASE_URL . '/views');
 define('DATA_URL', BASE_URL . '/data');
 define('ADMIN_URL', BASE_URL . '/admin');
